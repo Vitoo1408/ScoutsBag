328e9cce6791da9c52e928faea67615e1a169e46
nelson <a20142@alunos.ipca.pt>
2021-06-17 19:26:44 +0100

    Adicionei gif ao splash screen

--- app/src/main/java/pt/ipca/scoutsbag/Utils.kt
+++ app/src/main/java/pt/ipca/scoutsbag/Utils.kt
@@ -4,7 +4,10 @@ import android.app.AlertDialog
 import android.app.DatePickerDialog
 import android.app.TimePickerDialog
 import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
 import android.net.Uri
+import android.os.Build
 import android.provider.OpenableColumns
 import android.widget.TextView
 import com.example.scoutsteste1.ScoutActivity
@@ -332,6 +335,25 @@ object Utils {
 
         return sb.toString()
     }
+
+
+    fun isOnline(context: Context): Boolean {
+        val cm = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            val n = cm.activeNetwork
+            if (n != null) {
+                val nc = cm.getNetworkCapabilities(n)
+                //It will check for both wifi and cellular network
+                return nc!!.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) || nc.hasTransport(NetworkCapabilities.TRANSPORT_WIFI)
+            }
+            return false
+        } else {
+            val netInfo = cm.activeNetworkInfo
+            return netInfo != null && netInfo.isConnectedOrConnecting
+        }
+    }
+
 }
 
 
