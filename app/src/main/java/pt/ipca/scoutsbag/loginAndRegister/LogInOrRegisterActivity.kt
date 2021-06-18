9474aeaf888435d28a7ed6d5fc89f6bb5aac1953
nelson <a20142@alunos.ipca.pt>
2021-06-18 20:20:55 +0100

    bug ao limpar as activities se um utilizador já estiver logado

--- app/src/main/java/pt/ipca/scoutsbag/loginAndRegister/LogInOrRegisterActivity.kt
+++ app/src/main/java/pt/ipca/scoutsbag/loginAndRegister/LogInOrRegisterActivity.kt
@@ -1,12 +1,9 @@
 package pt.ipca.scoutsbag.loginAndRegister
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.widget.Button
-import org.json.JSONObject
-import org.json.JSONTokener
+import androidx.appcompat.app.AppCompatActivity
 import pt.ipca.scoutsbag.MainActivity
 import pt.ipca.scoutsbag.R
 
@@ -24,8 +21,8 @@ class LogInOrRegisterActivity : AppCompatActivity() {
 
         if(loggedIn == "true"){
             val intent = Intent(this, MainActivity::class.java)
-            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
             startActivity(intent)
+            finishAffinity()
         }
 
         findViewById<Button>(R.id.bt_Register).setOnClickListener() {
